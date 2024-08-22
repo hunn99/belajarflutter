cc091f32bc71443538075c1bd16912192fba2d0b
hunn99 <dennymalikibrahim@gmail.com>
2024-08-22 20:57:27 +0700

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Belajar Flutter"),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Belajar Flutter"),
+          ),
+          body: Container(
+            color: Colors.black,
+            margin: const EdgeInsets.fromLTRB(10, 15, 20, 25),
+            padding: EdgeInsets.only(bottom: 50, top: 50),
+            child: Container(
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                      begin: AlignmentDirectional.topStart,
+                      end: AlignmentDirectional.bottomEnd,
+                      colors: <Color>[Colors.blue, Colors.purple])),
             ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Text 1"),
-                Text("Text 2"),
-                Text("Text 3"),
-                Row(
-                  children: [
-                    Text("Text 4"),
-                    Text("Text 5"),
-                    Text("Text 6"),
-                  ],
-                )
-              ],
-            )));
+          )),
+    );
   }
 }
