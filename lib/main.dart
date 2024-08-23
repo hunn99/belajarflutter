8d74038eb22c7b2b8b21f706715a2aba0b1e7193
hunn99 <dennymalikibrahim@gmail.com>
2024-08-23 22:54:04 +0700

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,49 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Belajar Flutter"),
-          ),
-          body: Container(
-            color: Colors.black,
-            margin: const EdgeInsets.fromLTRB(10, 15, 20, 25),
-            padding: EdgeInsets.only(bottom: 50, top: 50),
-            child: Container(
-              margin: EdgeInsets.all(50),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                      begin: AlignmentDirectional.topStart,
-                      end: AlignmentDirectional.bottomEnd,
-                      colors: <Color>[Colors.blue, Colors.purple])),
+        home: Scaffold(
+      appBar: AppBar(
+          title: const Text('Stateful Widgets'), backgroundColor: Colors.blue),
+      body: Center(
+        child: Column(
+          children: [
+            Text(_counter.toString()),
+            ElevatedButton(
+              onPressed: _increment,
+              child: Text("tambah angka"),
             ),
-          )),
-    );
+            ElevatedButton(onPressed: _decrement, child: Text("kurangi angka"))
+          ],
+        ),
+      ),
+    ));
   }
 }
